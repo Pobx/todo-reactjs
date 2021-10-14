610cc4d1a68d11b43de30b97f84a303596ddfe4b
Pobx <geidtiphong.s@allrightbox.com>
2021-10-14 14:59:56 +0700

    remove console.log and add style line-through by condition true or false

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,16 @@
-
-function TodoList({todos}) {
-  console.log(todos)
+function TodoList({ todos }) {
   return (
     <ul>
-      {todos.map((todo, index) => (<li key={index}>Id: {todo.id} text: {todo.text} done: {todo.done}</li>))}
+      {todos.map((todo, index) => (
+        <li
+          key={index}
+          style={{
+            textDecoration: todo.done ? "line-through" : "",
+          }}
+        >
+          Id: {todo.id} text: {todo.text} done: {todo.done}
+        </li>
+      ))}
     </ul>
   );
 }
