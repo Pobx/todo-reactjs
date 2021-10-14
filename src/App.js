7baab8b9cbe950a420ba3249c0e4c95014b1eaee
Pobx <geidtiphong.s@allrightbox.com>
2021-10-14 14:46:41 +0700

    add React.useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 // import './App.css';
 import TodoList from "./TodoList";
 import AddTodo from "./AddTodo";
-
-const todos = [
-  { id: 1, text: "A 1", done: false },
-  { id: 2, text: "A 2", done: false },
-  { id: 3, text: "A 3", done: false },
-  { id: 4, text: "A 4", done: false },
-  { id: 5, text: "A 5", done: false },
-];
+import React from 'react';
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, text: "A 1", done: false },
+    { id: 2, text: "A 2", done: false },
+    { id: 3, text: "A 3", done: false },
+    { id: 4, text: "A 4", done: false },
+    { id: 5, text: "A 5", done: false },
+  ]);
+
   return (
     <div className="App">
       <h1>Todo List</h1>
       <TodoList todos={todos} />
-      <AddTodo />
+      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
